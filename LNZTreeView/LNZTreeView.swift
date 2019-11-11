bfa29381b6850b0123a4ef461ab103638c25ba6c
Rehan Uddin <rehan@posapptive.com>
2019-11-11 11:31:33 +0000

    remove select on toggle

--- LNZTreeView/LNZTreeView.swift
+++ LNZTreeView/LNZTreeView.swift
@@ -152,7 +152,7 @@ public class LNZTreeView: UIView {
         return nodesForSection[section]?.filter( { parent?.identifier == $0.parent?.identifier }).count ?? 0
     }
     
-    private func toggleExpanded(_ toggle: Bool, node: TreeNodeProtocol, inSection section: Int, select: Bool) -> Bool {
+    private func toggleExpanded(_ toggle: Bool, node: TreeNodeProtocol, inSection section: Int) -> Bool {
         guard node.isExpandable,
             let nodes = nodesForSection[section],
             let indexPath = indexPathForNode(node, inSection: section) else {
@@ -161,9 +161,7 @@ public class LNZTreeView: UIView {
         
         let minimalNode = nodes[indexPath.row]
         guard minimalNode.isExpanded != toggle else { return true }
-        if(select){
-            tableView(tableView, didSelectRowAt: indexPath)
-        }
+        tableView(tableView, didSelectRowAt: indexPath)
         return minimalNode.isExpanded == toggle
     }
     
@@ -175,7 +173,7 @@ public class LNZTreeView: UIView {
      */
     @discardableResult
     public func expand(node: TreeNodeProtocol, inSection section: Int) -> Bool {
-        return toggleExpanded(true, node: node, inSection: section, select: false)
+        return toggleExpanded(true, node: node, inSection: section)
     }
     
     /**
@@ -186,8 +184,8 @@ public class LNZTreeView: UIView {
      - returns: true if the node was successfully collapsed, false otherwise.
      */
     @discardableResult
-    public func collapse(node: TreeNodeProtocol, inSection section: Int, select: Bool) -> Bool {
-        return toggleExpanded(false, node: node, inSection: section, select: select)
+    public func collapse(node: TreeNodeProtocol, inSection section: Int) -> Bool {
+        return toggleExpanded(false, node: node, inSection: section)
     }
     
     /**
